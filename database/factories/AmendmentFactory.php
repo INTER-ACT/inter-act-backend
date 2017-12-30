1816fa545801a58593140f336e5364448471da63
Manuel <manuel.kirchner89@gmail.com>
2017-12-30 15:24:27 +0100

    resolve Amendment creation bug

--- database/factories/AmendmentFactory.php
+++ database/factories/AmendmentFactory.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Discussions\Discussion;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(\App\Amendments\Amendment::class, function (Faker $faker) {
-    $user = \App\User::First();
-    $discussion = \App\Discussions\Discussion::First();
+    $user = factory(User::class)->create();
+    $discussion = factory(Discussion::class)->create(['user_id' => $user->id]);
     return [
         'discussion_id' => $discussion->id,
         'user_id' => $user->id,
-        'updated_text' => $discussion->law_text . 'Amendment Additional Text',
-        'explanation' => "Test Explanation of Amendment"
+        'updated_text' => $discussion->law_text . 'AmendmentResource Additional Text',
+        'explanation' => "Test Explanation of AmendmentResource"
     ];
 });
