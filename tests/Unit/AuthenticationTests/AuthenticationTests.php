b7781171949d71d3eaa39c814650fe79b956d4db
Manuel <manuel.kirchner89@gmail.com>
2018-01-05 17:55:54 +0100

    configure passport

---
+++ tests/Unit/AuthenticationTests/AuthenticationTests.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Passport\Passport;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthenticationTests extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testServerCreation()
+    {
+        Passport::actingAs(
+            factory(User::class)->create(), ['*']
+        );
+
+        $response = $this->get('/ping');
+
+        $response->assertStatus(200);
+    }
+}
